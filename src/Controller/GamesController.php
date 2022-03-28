789be1ee24f61ffd4f102fa13ad95812f06c698b
Edgar Pureco <pureco.edgar.1am@gmail.com>
2022-03-28 15:45:53 +0200

    BD Commit

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -1,18 +1,24 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\GamesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GamesController extends AbstractController
 {
+
+    public function __construct(GamesRepository $gamesRepository) {
+		$this->gamesRepository = $gamesRepository;
+	}
+
     #[Route('/games', name: 'app_games')]
     public function index(): Response
     {
         return $this->render('games/index.html.twig', [
             'controller_name' => 'GamesController',
+            'games' => $this->gamesRepository->findAll()
         ]);
     }
 }
