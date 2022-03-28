145f81a7f1271474c58f1353628577f9a762a809
Edgar Pureco <pureco.edgar.1am@gmail.com>
2022-03-28 15:17:20 +0200

    BD Commit

--- blogSymfony.sql
+++ blogSymfony.sql
@@ -46,6 +46,25 @@ CREATE TABLE IF NOT EXISTS `type` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Structure de la table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`  ;
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Structure de la table `plateforme`
+--
+DROP TABLE IF EXISTS `plateforme`;
+CREATE TABLE IF NOT EXISTS `plateforme` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
 -- Structure de la table `comment`
 --
 DROP TABLE IF EXISTS `comment` ;
@@ -59,18 +78,18 @@ CREATE TABLE IF NOT EXISTS `comment` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `game`
+-- Structure de la table `games`
 --
-DROP TABLE IF EXISTS `game` ;
-CREATE TABLE IF NOT EXISTS `game` (
+DROP TABLE IF EXISTS `games` ;
+CREATE TABLE IF NOT EXISTS `games` (
   `id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `year` date NOT NULL,
   `developer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `synopsis` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `plateforme` longtext COLLATE utf8mb4_unicode_ci NOT NULL
+  `plateforme_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -147,6 +166,24 @@ INSERT INTO `type` (`id`, `name`) VALUES
 (3, 'Produit');
 
 --
+-- Déchargement des données de la table `plateforme`
+--
+
+INSERT INTO `plateforme` (`id`, `name`) VALUES
+(1, 'Xbox Series'),
+(2, 'PS5'),
+(3, 'PS4'),
+(4, 'One'),
+(5, 'Switch'),
+(6, 'PC'),
+(7, 'Xbox Series/One'),
+(8, 'PS5/PS4'),
+(9, 'PC/PS5/PS4'),
+(10, 'PC'),
+(11, 'PC/Xbox Series/One'),
+(12, 'PC/Xbox Series/One/PS5/PS4');
+
+--
 -- Déchargement des données de la table `category`
 --
 
@@ -167,26 +204,30 @@ INSERT INTO `product` (`id`, `category_id`, `name`, `photo`, `price`, `descripti
 (2, 4, 'Porte-clé', 'upload/62415d42c12d91.71829870.jpg', 5, 'Porte-clé du site');
 
 --
--- Déchargement des données de la table `game`
+-- Déchargement des données de la table `genre`
 --
-INSERT INTO `game` (`id`, `category_id`, `title`, `year`, `developer`, `synopsis`, `photo`) VALUES
-(1, 1, 'Halo Infinite', '2021-11-15', "343 Industries", "Incarnez le Major :
-Tandis que l'espoir meurt et que le sort de l'humanité est en jeu, le Major s'apprête à affronter son plus redoutable ennemi à ce jour. Enfilez l'armure du plus grand héros de l'humanité pour vivre une aventure épique et explorer l'immensité de l'anneau.
-Explorez le Halo Zeta :
-Halo Infinite offre la campagne Halo la plus complète et haletante jamais proposée, située dans l'environnement ouvert d'un anneau-monde antique, le Halo Zeta. Parcourez les vastes étendues de l'Installation 07, des points culminants aux profondeurs secrètes de l'anneau. Sauvez les Marines de l'UNSC pour obtenir des renforts dans votre lutte contre ces redoutables ennemis qui se font appeler ''les Parias''.
-Terrassez les Parias :
-Suite aux événements de Halo 5 et après avoir essuyé une défaite écrasante, la flotte de l'UNSC s'est échouée à la surface de l'Installation 07, fortement endommagée. Mais un clan terrifiant de guerriers brutes, les Parias, arpente désormais cet anneau-monde dévasté. En claire infériorité numérique et matérielle, le Major devra empêcher les Parias de réparer le Halo et d'avoir la mainmise sur la plus grande menace de la galaxie.", NULL),
-(2, 1, 'Elden Ring', '2022-02-25', "FromSoftware", "UNE NOUVELLE AVENTURE GRANDIOSE VOUS ATTEND
-Levez-vous, Sans-éclat, et puisse la grâce guider vos pas. Brandissez la puissance du Cercle d'Elden. Devenez Seigneur de l'Entre-terre.
-• Un vaste monde à explorer
-Un monde immense aux environnements riches et variés, parsemé d'obscurs et tortueux donjons tous reliés naturellement entre eux, vous attend. Au fil de votre exploration, goûtez à l'inconnu, bravez les menaces permanentes et accomplissez votre destinée.
-• Créez votre propre personnage
-En plus de l'apparence de votre personnage, vous êtes libre de personnaliser votre arsenal d'armes, d'armures et de sorts. Construisez un personnage qui correspond à votre style de jeu, et devenez un guerrier surpuissant, ou encore un maître de la magie.
-• Un scénario épique né d'un mythe
-Plusieurs histoires se mêlent dans un récit narré par fragments. Vous rencontrerez des personnages complexes au fil de votre découverte épique de l'Entre-terre.
-• Connectez-vous librement à d'autres joueurs grâce au mode en ligne
-En plus du mode multijoueur, vous permettant de vous connecter directement à d'autres joueurs et de voyager ensemble, le jeu propose un système en ligne asynchrone unique vous permettant de ressentir la présence des autres Sans-éclat.", NULL ),
-(3, 3, 'Minecraft', '2011-11-18',"Mojang", "Minecraft est un jeu vidéo de type aventure « bac à sable » (construction complètement libre) développé par le Suédois Markus Persson, alias Notch, puis par la société Mojang Studios. Il s'agit d'un univers composé de voxels et généré de façon procédurale, qui intègre un système d'artisanat axé sur l'exploitation puis la transformation de ressources naturelles (minéralogiques, fossiles, animales et végétales).", NULL);
+
+INSERT INTO `genre` (`id`, `name`) VALUES
+(1, 'FPS'),
+(2, 'RPG'),
+(3, 'Survival'),
+(4, 'Action'),
+(5, 'Aventure'),
+(6, 'Plateforme'),
+(7, 'Simulation'),
+(8, 'Sport');
+
+--
+-- Déchargement des données de la table `games`
+--
+INSERT INTO `games` (`id`, `title`, `year`, `developer`, `synopsis`, `photo`, `plateforme_id`, `genre_id`) VALUES
+(1, 'Dying Light 2 : Stay Human', '2022-02-04', 'Techland', 'Dying Light 2 est un Survival-Horror dans lequel le joueur doit survivre dans un environnement peuplé de zombies. Dans ce nouvel opus, l\'enjeu sera de contrôler notamment des réserves d\'eau et de nourriture afin de maîtriser une cité de survivants. Les choix du joueur ont ainsi une importance capitale dans le déroulement du scénario.\r\n', 'https://image.jeuxvideo.com/medias-sm/163247/1632473272-6800-jaquette-avant.jpg ', 2, 3),
+(2, 'Battlefield 2042', '2021-11-19', 'Dice  Electronic Arts', 'Battlefield 2042 est un jeu de tir à la première personne multijoueur ancré dans un futur dystopique. Deux nations mènent une guerre totale, les Etats-Unis et la Russie. Trois modes de jeux sont proposés : All-Out Warfare (128 joueurs sur PS5/Xbox Series, 64 sur PS4/Xbox One), Hazard Zone et Mode Portal.', 'https://image.jeuxvideo.com/medias-sm/163664/1636636223-8849-jaquette-avant.jpg', 4, 1),
+(3, 'Fifa 22', '2021-10-01', '', 'FIFA 22 est une simulation de football éditée par Electronic Arts. Comme chaque saison, le jeu offre son lot d\'améliorations techniques pour toujours plus de réalisme ainsi que des animations et des comportements toujours plus poussés. Les modes carrière et Ultimate Team disposent également de nouveaux ajouts.', 'https://image.jeuxvideo.com/medias-sm/163154/1631541998-5162-jaquette-avant.jpg', 12, 8),
+(4, 'Légendes Pokémon : Arceus', '2022-01-22', 'Nintendo The Pokémon Company', 'Légendes Pokémon : Arceus tranche avec les précédents opus Pokémon puisqu\'il prend place dans un monde ouvert. Le titre se déroule dans la région de Sinnoh, bien avant sa colonisation par les êtres humains. Le joueur incarnera toujours un dresseur de Pokémon, cette fois-ci chargé de créer le premier Pokédex de Sinnoh.', 'https://image.jeuxvideo.com/medias-sm/163353/1633531106-3464-jaquette-avant.gif', 5, 4),
+(5, 'Elden Ring', '2022-02-25', 'From Software Bandai Namco', 'Elden Ring est le nouveau jeu de From Software. Il s\'agit d\'un RGP/action à la troisième personne qui proposerait un monde ouvert. L ejeu marque la collaboration entre Hidetaka Miyazaki et George R. R. Martin, le créateur de Game of Thrones.', 'https://image.jeuxvideo.com/medias-sm/163301/1633006670-7022-jaquette-avant.jpg', 12, 2),
+(6, 'Martha is Dead', '2022-02-24', '	LKA Wired Productions', 'Aventure à la première personne, Martha is Dead est un thriller psychologique dont l\'intrigue commence en 1944, en Toscane. Le conflit entre l\'Allemagne et les Alliés y fait rage. À travers les yeux d\'une fille d\'un soldat allemand, le joueur découvre le corps d\'une femme noyée : il devra donc partir à la recherche de la vérité entourant le mystérieux décès.', 'https://image.jeuxvideo.com/medias-sm/164545/1645450819-855-jaquette-avant.jpg', 1, 5),
+(7, 'New Super Mario Bros. U Deluxe', '2019-01-11', 'Nintendo', 'New Super Mario Bros. U Deluxe est à la fois une fusion et un portage. Le jeu contient tous les niveaux de New Super Mario Bros. U et New Super Luigi. U emmenés sur Nintendo Switch. Les personnages secondaires ont également été modifiées par rapport au jeu de base, Nabbit et Toadette sont donc les aventuriers bonus de cet épisode.', 'https://image.jeuxvideo.com/medias-sm/154410/1544104487-917-jaquette-avant.jpg', 5, 6);
 
 
 --
@@ -212,7 +253,6 @@ Avec les mises à jour de Halo Infinite traditionnellement déployées à 10 h P
 « Nous avons pris la décision de prolonger la saison 1 pour nous donner plus de temps pour nous assurer que la saison 2 respecte notre barre de haute qualité et ainsi nous pouvons terminer le développement de la saison 2 de manière saine et durable pour notre équipe », a déclaré Joesph Staten, directeur créatif de Halo Infinite. . Le déménagement a vu la saison 1 s’étendre sur six mois, avec plus d’événements introduits pour compenser la durée d’exécution prolongée.","2022-03-28"),
 (2, 6, 1, "Le Warden est là grace à la Snapshot 22w12a", "Depuis le 24 mars dernier, une nouvelle Snapshot est disponible dans Minecraft et il s'agit de la Snapshot 22w12a. Celle-ci permet l'ajout de nouveautés avec principalement l'introduction du Warden, mais aussi les hurleurs Sculk, ainsi qu'un nouvel enchantement et les bateaux-coffre.", "2022-03-27");
 -- --------------------------------------------------------
-
 
 --
 -- Index pour les tables déchargées
@@ -240,11 +280,18 @@ ALTER TABLE `comment`
   ADD KEY `IDX_9474526CA76ED395` (`user_id`);
 
 --
--- Index pour la table `game`
+-- Index pour la table `games`
 --
-ALTER TABLE `game`
+ALTER TABLE `games`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_FF232B3112469DE2` (`category_id`);
+  ADD KEY `IDX_FF232B31391E226B` (`plateforme_id`),
+  ADD KEY `IDX_FF232B314296D31F` (`genre_id`);
+
+--
+-- Index pour la table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `orders`
@@ -260,6 +307,12 @@ ALTER TABLE `order_line`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_9CE58EE14584665A` (`product_id`),
   ADD KEY `IDX_9CE58EE18D9F6D38` (`order_id`);
+
+--
+-- Index pour la table `plateforme`
+--
+ALTER TABLE `plateforme`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `product`
@@ -297,10 +350,16 @@ ALTER TABLE `comment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT pour la table `game`
+-- AUTO_INCREMENT pour la table `games`
 --
-ALTER TABLE `game`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `orders`
@@ -313,6 +372,12 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `order_line`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `plateforme`
+--
+ALTER TABLE `plateforme`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `product`
@@ -344,10 +409,11 @@ ALTER TABLE `comment`
   ADD CONSTRAINT `FK_9474526CA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
--- Contraintes pour la table `game`
+-- Contraintes pour la table `games`
 --
-ALTER TABLE `game`
-  ADD CONSTRAINT `FK_FF232B3112469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+ALTER TABLE `games`
+  ADD CONSTRAINT `FK_FF232B31391E226B` FOREIGN KEY (`plateforme_id`) REFERENCES `plateforme` (`id`),
+  ADD CONSTRAINT `FK_FF232B314296D31F` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`);
 
 --
 -- Contraintes pour la table `orders`
