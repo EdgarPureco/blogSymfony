863513e81ecf2a85c7683d3a1c0d8b6701bd2851
Aymane Silini <silinia@iut1i-702-a04.u-ga.fr>
2022-03-28 17:29:23 +0200

    game final v1

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -1,24 +1,38 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\Games;
 use App\Repository\GamesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/games")]
+
 class GamesController extends AbstractController
 {
+
+    private GamesRepository $gamesRepository;
+
 
     public function __construct(GamesRepository $gamesRepository) {
 		$this->gamesRepository = $gamesRepository;
 	}
 
-    #[Route('/games', name: 'app_games')]
+
+    #[Route("/", name: "games_index", methods: ["GET"])]
     public function index(): Response
     {
         return $this->render('games/index.html.twig', [
-            'controller_name' => 'GamesController',
             'games' => $this->gamesRepository->findAll()
         ]);
     }
+
+    #[Route("/{id}", name: "games_get", methods: ["GET"])]
+	public function getById(Games $game): Response {
+		return $this->render('games/get.html.twig', [
+			'game' => $game
+		]);
+	}
 }
